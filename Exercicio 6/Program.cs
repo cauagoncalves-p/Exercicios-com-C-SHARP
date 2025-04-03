cee76021e53747492cc1e07afd3a1bd434209177
cauagoncalves-p <cauagoncalves2190@gmail.com>
2025-04-03 17:21:01 -0300

    ex 6

---
+++ Exercicio 6/Program.cs
@@ -0,0 +1,54 @@
+﻿// Elabore um programa em C# para realizar o cadastro de alunos em 3 sistemas escolares
+// incluindo nome, matrícula, idade e curso. O programa deve permitir o cadastro de até 30 alunos e exibir a média de
+// informações após o registro, além de informar se o estudante foi aprovado ou não, levando em consideração o corte de
+// notas passadas pelo sistema.
+
+using System.Xml;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int notaCorte;
+        string A, B, C, nome, matricula, curso;
+        int idade;
+        bool cadastrar = true;
+        Console.Write("Qual sistema será feito o cadastro do aluno ? Cada sistema tem sua própria nota de corte!\nSistemas : A, B, C - (Sair)\nResposta: ");
+        string resposta = Console.ReadLine().ToUpper();
+
+
+        while (cadastrar) 
+        {
+            for (int i = 0; i <= 30; i++)
+            {
+                Console.Write($"Informações do {i + 1} aluno");
+                Console.WriteLine();
+                Console.Write("Nome: "); nome = Console.ReadLine();
+                Console.Write("Matricula: "); matricula = Console.ReadLine();
+                Console.Write("Idade: "); Convert.ToInt32(Console.ReadLine());
+                Console.Write("Curso: "); curso = Console.ReadLine();
+
+                Console.WriteLine();
+
+                Console.Write("Qual sistema será feito o cadastro do aluno ? Cada sistema tem sua própria nota de corte!\nSistemas : A, B, C - (Sair)\nResposta: ");
+                resposta = Console.ReadLine().ToUpper();
+                i++;
+
+                if (resposta == "Sair") ; cadastrar=false;
+            }
+        }
+
+        switch (resposta)
+        {
+            case "A":
+                notaCorte = 150;
+                break;
+            case "B":
+                notaCorte = 180;
+                break;
+            case "C":
+                notaCorte = 200;
+                break;
+        }
+    }
+}
--- exercicio 5/Program.cs
+++ exercicio 5/Program.cs
@@ -3,17 +3,16 @@
 // a cada cigarro, e calcule quantos dias de vida um fumante perderá.
 // Exiba o total em dias.
 
-Console.Write("Quantos cigarros você fuma por dia?");
+Console.Write("Quantos cigarros você fuma por dia:? ");
 int contagemCigarros = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Por quanto tempo fuma?");
-double tempoFumante = Convert.ToDouble(Console.ReadLine());
+Console.Write("Por quantos anos você fumou:? ");
+double tempoFumando = Convert.ToDouble(Console.ReadLine());
 
 // Convertendo a quntidade de cigarros fumados por dia para minutos
-double minutosFumados = contagemCigarros * (10 * 60);
+double minutosFumados = contagemCigarros * 10;
 // Sabendo quanto tempo ele perdeu em minutos
-double tempoPerdido = minutosFumados * tempoFumante;
-
+double tempoPerdido = (minutosFumados * 365) * tempoFumando;
 double diasPerdido = tempoPerdido / 1440;
 
 Console.Write($"Voce fuma {contagemCigarros} cigarros por dia! Durante todo o tempo que você informou que fuma" +
-    $"\n Durante esse tempo você perdeu {diasPerdido} dias de vida.");
+    $"\n Você perdeu {diasPerdido:f1} dias de vida.");
